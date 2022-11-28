c7253fd8dd3a84cfa02d4c520c97fead5bb9ee69
itzik <itzik3877@gmail.com>
2022-11-28 20:46:28 +0200

    all work exept 6 need to adapt 1-5 to the code

--- shell.c
+++ shell.c
@@ -10,7 +10,7 @@
  * 
  */
 
-#include <unistd.h>
+// #include <unistd.h>
 #include <fcntl.h>
 
 #include <stdio.h>
@@ -31,6 +31,14 @@
 
 // #include "Shell.h"
 
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<readline/readline.h>
+#include<readline/history.h>
 /* method to clear the shell. */
 #define clear() printf("\033[H\033[J]")
 
@@ -38,6 +46,14 @@
 #define GREEN "\033[0;32m"
 #define PURPLE "\033[0;35m"
 #define WHITE "\033[0m"
+
+/*
+use with redirection(< > >>) to indicate to the function in which mode to open the file
+and to know that redirection of the input OR output has to be done
+*/
+#define INPUT 0
+#define OUTPUT 1
+#define APPEND 2
 
 /* Globals */
 #define TRUE 1
@@ -116,136 +132,131 @@ void user_input(char *input){
         return;
         
 }
-// function for finding pipe
-int parsePipe(char* str, char** strpiped)
-{
-    int i;
-    for (i = 0; i < 2; i++) {
-        strpiped[i] = strsep(&str, "|");
-        if (strpiped[i] == NULL)
-            break;
-    }
-  
-    if (strpiped[1] == NULL)
-        return 0; // returns zero if no pipe is found.
-    else {
-        return 1;
-    }
+/*
+removes the newline and space character from the end and start of a char*
+*/
+void removeWhiteSpace(char* buf){
+	if(buf[strlen(buf)-1]==' ' || buf[strlen(buf)-1]=='\n')
+	buf[strlen(buf)-1]='\0';
+	if(buf[0]==' ' || buf[0]=='\n') memmove(buf, buf+1, strlen(buf));
 }
-// function for parsing command words
-void parseSpace(char* str, char** parsed)
-{
-    int i;
-  
-    for (i = 0; i < 100; i++) {
-        parsed[i] = strsep(&str, " ");
-  
-        if (parsed[i] == NULL)
-            break;
-        if (strlen(parsed[i]) == 0)
-            i--;
-    }
+/*
+tokenizes char* buf using the delimiter c, and returns the array of strings in param
+and the size of the array in pointer nr
+*/
+void tokenize_buffer(char** param,int *nr,char *buf,const char *c){
+	char *token;
+	token=strtok(buf,c);
+	int pc=-1;
+	while(token){
+		param[++pc]=malloc(sizeof(token)+1);
+		strcpy(param[pc],token);
+		removeWhiteSpace(param[pc]);
+		token=strtok(NULL,c);
+	}
+	param[++pc]=NULL;
+	*nr=pc;
 }
-  
-int processString(char* str, char** parsed, char** parsedpipe)
-{
-  
-    char* strpiped[2];
-    int piped = 0;
-  
-    piped = parsePipe(str, strpiped);
-    printf("piped %d \n", piped);
-    if (piped) {
-        parseSpace(strpiped[0], parsed);
-        parseSpace(strpiped[1], parsedpipe);
-  
-    } else {
-        
-        parseSpace(str, parsed);
-    }
-  
-    // if (ownCmdHandler(parsed))
-    //     return 0;
-    // else
-    return 1 + piped;
+/*
+loads and executes a series of external commands that are piped together
+*/
+void executePiped(char** buf,int nr){//can support up to 10 piped commands
+	if(nr>10) return;
+	
+	int fd[10][2],i,pc;
+	char *argv[100];
+
+	for(i=0;i<nr;i++){
+		tokenize_buffer(argv,&pc,buf[i]," ");
+		if(i!=nr-1){
+			if(pipe(fd[i])<0){
+				perror("pipe creating was not successfull\n");
+				return;
+			}
+		}
+		if(fork()==0){//child1
+			if(i!=nr-1){
+				dup2(fd[i][1],1);
+				close(fd[i][0]);
+				close(fd[i][1]);
+			}
+
+			if(i!=0){
+				dup2(fd[i-1][0],0);
+				close(fd[i-1][1]);
+				close(fd[i-1][0]);
+			}
+			execvp(argv[0],argv);
+			perror("invalid input ");
+			exit(1);//in case exec is not successfull, exit
+		}
+		//parent
+		if(i!=0){//second process
+			close(fd[i-1][0]);
+			close(fd[i-1][1]);
+		}
+		wait(NULL);
+	}
 }
-void execArgsPiped(char** parsed, char** parsedpipe)
-{
-    printf("174\n");
-    printf("parsed %s%s \n", parsed);
-    printf("parsedpipe %s%s \n", parsedpipe);
-    // exit(1);
-    // 0 is read end, 1 is write end
-    int pipefd[2]; 
-    pid_t p1, p2;
-    printf("177\n");
-    // exit(1);
-    if (pipe(pipefd) < 0) {
-        printf("184");
-        printf("\nPipe could not be initialized");
-        return;
-    }
-    p1 = fork();
-    if (p1 < 0) {
-        printf("189");
-        printf("\nCould not fork");
-        return;
-    }
-    // exit(1);
-    if (p1 == 0) {
-        printf("195");
-        // Child 1 executing..
-        // It only needs to write at the write end
-        // sleep(5);
-        exit(1);
-        printf("pipefd[0] %d ", pipefd[0]);
-        printf("pipefd[1] %d ", pipefd[1]);
-        sleep(5);
-        close(pipefd[0]);
-        dup2(pipefd[1], STDOUT_FILENO);
-        close(pipefd[1]);
-        printf("parsed[0] %s", parsed[0]);
-        printf("parsed %s%s ", parsed);
-        // exit(1);
-        if (execvp(parsed[0], parsed) < 0) {
-            printf("\nCould not execute command 1..");
-            exit(0);
-        }
-        // exit(1);
-    } else {
-        printf("201");
-        // Parent executing
-        p2 = fork();
-  
-        if (p2 < 0) {
-            printf("\nCould not fork");
-            return;
-        }
-  
-        // Child 2 executing..
-        // It only needs to read at the read end
-        if (p2 == 0) {
-            printf("212");
-            close(pipefd[1]);
-            dup2(pipefd[0], STDIN_FILENO);
-            close(pipefd[0]);
-            // printf("parsedpipe[0] %s ", parsedpipe[0]);
-            // printf("parsedpipe %s ", parsedpipe);
-            if (execvp(parsedpipe[0], parsedpipe) < 0) {
-                printf("219");
-                printf("\nCould not execute command 2..");
-                exit(0);
-            }
-        } else {
-            printf("222");
-            // parent executing, waiting for two children
-            wait(NULL);
-            wait(NULL);
-        }
-    }
-    // exit(1);
-    // return;
+
+/*
+loads and executes a series of external commands that have to be run asyncronously
+*/
+void executeAsync(char** buf,int nr){
+	int i,pc;
+	char *argv[100];
+	for(i=0;i<nr;i++){
+		tokenize_buffer(argv,&pc,buf[i]," ");
+		if(fork()==0){
+			execvp(argv[0],argv);
+			perror("invalid input ");
+			exit(1);//in case exec is not successfull, exit
+		}
+	}
+	for(i=0;i<nr;i++){
+		wait(NULL);
+	}
+
 }
+
+/*
+loads and executes a an external command that needs file redirection(input, output or append)
+*/
+void executeRedirect(char** buf,int nr,int mode){
+	int pc,fd;
+	char *argv[100];
+	removeWhiteSpace(buf[1]);
+	tokenize_buffer(argv,&pc,buf[0]," ");
+	if(fork()==0){
+
+		switch(mode){
+		case INPUT:  fd=open(buf[1],O_RDONLY); break;
+		case OUTPUT: fd=open(buf[1],O_WRONLY); break;
+		case APPEND: fd=open(buf[1],O_WRONLY | O_APPEND); break;
+		default: return;
+		}
+
+		if(fd<0){
+			perror("cannot open file\n");
+			return;
+		}
+
+		switch(mode){
+		case INPUT:  		dup2(fd,0); break;
+		case OUTPUT: 		dup2(fd,1); break;
+		case APPEND: 		dup2(fd,1); break;
+		default: return;
+		}
+		execvp(argv[0],argv);
+		perror("invalid input ");
+		exit(1);//in case exec is not successfull, exit
+	}
+	wait(NULL);
+}
+
+/*
+shows the internal help
+*/
 char *split_input[MAXSIZE];
 void manual_system_calls(char *input){//check what its do.
 
@@ -466,53 +477,128 @@ void print_echo_msg(char *return_echo){
     return_echo+=5;
     printf("%s",return_echo);
 }
-
+// Function to take input
+// int takeInput(char* str)
+// {
+//     char* buf;
+  
+//     buf = readline("\n>>> ");
+//     if (strlen(buf) != 0) {
+//         add_history(buf);
+//         strcpy(str, buf);
+//         return 0;
+//     } else {
+//         return 1;
+//     }
+// }
 void main(){
 
-    char inputstring[1000], *parsedArgs[100];
-    char* paredArgsPiped[100];
-    int execFlag = 0;
-    char *inputString;
-    inputString = (char*) malloc(MAXSIZE*sizeof(char));
-    shell_initialize();
-    // printf("%s", "380");
-    struct utsname os_type;
-    uname(&os_type); // name finds.
+    char buf[500],*buffer[100],buf2[500],buf3[500], *params1[100],*params2[100],*token,cwd[1024];
+	int nr=0;
+	printf("*****************************************************************"    "\n");
+	printf("**************************CUSTOM SHELL***************************"    "\n");
 
-    while(TRUE){
+	while(1){
+		printf("Enter command(or 'exit' to exit):""\n");
+
+		//print current Directory
+		if (getcwd(cwd, sizeof(cwd)) != NULL)
+		printf( "%s  " , cwd);
+		else 	perror("getcwd failed\n");
+
+		//read user input
+		fgets(buf, 500, stdin);//buffer overflow cannot happen
+
+		//check if only a simple command needs to be executed or multiple piped commands or other types
+		if(strchr(buf,'|')){//tokenize pipe commands
+			tokenize_buffer(buffer,&nr,buf,"|");
+			executePiped(buffer,nr);
+		}
+		else if(strchr(buf,'&')){//asyncronous execution
+			tokenize_buffer(buffer,&nr,buf,"&");
+			executeAsync(buffer,nr);
+		}
+		else if(strstr(buf,">>")){//append output to file
+			tokenize_buffer(buffer,&nr,buf,">>");
+			if(nr==2)executeRedirect(buffer,nr,APPEND);
+			else printf("Incorrect output redirection!(has to to be in this form: command >> file)");
+		}
+		else if(strchr(buf,'>')){//redirect output to file
+			tokenize_buffer(buffer,&nr,buf,">");
+			if(nr==2)executeRedirect(buffer,nr, OUTPUT);
+			else printf("Incorrect output redirection!(has to to be in this form: command > file)");
+		}
+		else if(strchr(buf,'<')){//redirect file to input
+			tokenize_buffer(buffer,&nr,buf,"<");
+			if(nr==2)executeRedirect(buffer,nr, INPUT);
+			else printf("Incorrect input redirection!(has to to be in this form: command < file)");
+		}
+		else{//single command including internal ones
+			tokenize_buffer(params1,&nr,buf," ");
+			if(strstr(params1[0],"cd")){//cd builtin command
+				chdir(params1[1]);
+			}
+			// else if(strstr(params1[0],"help")){//help builtin command
+			// 	showHelp();
+			// }
+			else if(strstr(params1[0],"exit")){//exit builtin command
+				exit(0);
+			}
+			// else executeBasic(params1);
+		}
+	}
+
+	return 0;
+
+
+
+    // char inputstring[1000], *parsedArgs[100];
+    // char* paredArgsPiped[100];
+    // int execFlag = 0;
+    // char *inputString;
+    // inputString = (char*) malloc(MAXSIZE*sizeof(char));
+    // shell_initialize();
+    // // printf("%s", "380");
+    // struct utsname os_type;
+    // uname(&os_type); // name finds.
+
+    // while(TRUE){
   
-        printf("%s",getenv("USER")); // username.
-        printf("@%s",os_type.nodename);
-        printf(":");
-        printf("~");
-        printf("$ ");
+    //     printf("%s",getenv("USER")); // username.
+    //     printf("@%s",os_type.nodename);
+    //     printf(":");
+    //     printf("~");
+    //     printf("$ ");
 
-        // printf("inputstring%s ", inputstring);
-        // printf("parsedArgs%s%s ", parsedArgs);
-        // printf("pa")
+    //     // printf("inputstring%s ", inputstring);
+    //     // printf("parsedArgs%s%s ", parsedArgs);
+    //     // printf("pa")
         
-        bzero(inputString, strlen(inputString));
-        fgets(inputString,MAXSIZE,stdin);
-        inputString[strcspn(inputString,"\n")] = 0;
-        printf("inputString %s\n", inputString);
-        execFlag = processString(inputString, parsedArgs, paredArgsPiped);
-        printf("parsedArgs %s\n", parsedArgs);
-        printf("paredArgsPiped %s\n ", paredArgsPiped);
-        printf("execFlag %d \n", execFlag);
-        if (execFlag == 2)
-        {
-            printf("535\n");
-            execArgsPiped(parsedArgs, paredArgsPiped);
-            printf("484");
-            exit(1);
-            return;
-            // continue;;
-        }
-        if(execFlag == 1){
-            printf("493");
-            user_input(inputString);
-        }
-    }
-    free(inputString);
+    //     bzero(inputString, strlen(inputString));
+    //     fgets(inputString,MAXSIZE,stdin);
+        
+    //     inputString[strcspn(inputString,"\n")] = 0;
+    //     // printf("inputString %s\n", inputString);
+    //     execFlag = processString(inputString, parsedArgs, paredArgsPiped);
+    //     // if(takeInput(inputstring))
+    //     //     continue;
+    //     // printf("parsedArgs %s\n", parsedArgs);
+    //     // printf("paredArgsPiped %s\n ", paredArgsPiped);
+    //     // printf("execFlag %d \n", execFlag);
+    //     if (execFlag == 2)
+    //     {
+    //         printf("535\n");
+    //         execArgsPiped(parsedArgs, paredArgsPiped);
+    //         // printf("484");
+    //         // exit(1);
+    //         // return;
+    //         // continue;;
+    //     }
+    //     else{
+    //         printf("493");
+    //         user_input(inputString);
+    //     }
+    // }
+    // free(inputString);
 
 }
